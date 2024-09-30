d623a372caa3c0ec13cca368d062a6ea9f9b57d0
mulf <mail@mulf.wtf>
2024-09-30 16:43:26 -0500

    api server setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Json, routing::get, Router};
+
+use serde_json::{json, Value};
+
+async fn root() -> Json<Value> {
+    Json(json!({"data": 42}))
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("server running on port 3k");
+    axum::serve(listener, app).await.unwrap();
 }
